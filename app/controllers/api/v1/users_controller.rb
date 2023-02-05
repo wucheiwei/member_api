7a79b8e82ec050db75abbcbf0f2b74bbe6e77f84
zack <twd1000015706@gmail.com>
2023-02-05 18:52:08 +0800

    api_finished

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UsersController < ApplicationController
+    def index
+
+    end
+    def create
+      @user =User.new(user_params)
+    
+      if @user.save
+        render json: {susses: true}
+      else
+        render json:{susses: false}
+      end
+    
+    end
+    
+    def login
+      @user= User.new
+    end
+    private
+    def user_params
+      params.require(:user).permit(:username,:password)
+    end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    def current_user
+        @__keep_login__ ||= User.find_by(id: seesion[:keep_login])
+      end
+      def user_singed_in?
+        !!session[:keep_login]
+      end
 end
